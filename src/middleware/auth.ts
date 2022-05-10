fe27b261647ad9ea14006bd72f6189a25a90de8d
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-05-10 04:53:29 +0100

    UPDATE

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,15 +2,13 @@ import { Request, Response, NextFunction } from "express";
 import { User } from "../models/Users";
 
 export class AuthenticationMiddleware {
-    static async confirmAuthenticationStatus(req: Request, res: Response, next: NextFunction) {
-        // const { user_id, username } = req.session.user;
-        if (!req.session.user) {
-          return  res.redirect("/login")
-        }
-        const { user_id, username } = req.session.user;
-        const user = await User.findOne({ where: { user_id: user_id } });
-        req.user = user;
-        next();
-        return;
+  static async confirmAuthenticationStatus(req: Request, res: Response, next: NextFunction) {
+    if (!req.session.user) {
+      return res.redirect("/login")
     }
+    const { user_id, username } = req.session.user;
+    const user = await User.findOne({ where: { user_id: user_id } });
+    next();
+    return;
+  }
 }
