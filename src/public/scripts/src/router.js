c7df1775c90701ffe374c5d25dab8b37a49fdb1a
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-05-16 21:06:20 +0100

    FEAT :: added vue router

--- src/public/scripts/src/router.js
+++ src/public/scripts/src/router.js
@@ -5,8 +5,8 @@
 // These can be imported from other files
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
-const Baz = { template: "<div>baz</div>" }
-// 2. Deine some routes
+const Bax = {template:"'<div>baz</div>' "}
+// 2. Define some routes
 // Each route should map to a component. The "component" can
 // either be an actual component constructor created via
 // `Vue.extend()`, or just a component options object.
@@ -20,18 +20,6 @@ const routes = [
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
-const router = new VueRouter({
+ const  router = new VueRouter({
     routes // short for `routes: routes`
 })
-
-
-const app = new Vue({
-    router,
-    vuetify: new Vuetify(),
-    data() {
-        return {
-            name: "drizzle",
-            age: 67,
-        };
-    },
-}).$mount('#app')
