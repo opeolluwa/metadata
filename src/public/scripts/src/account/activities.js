1f7d60bd025bd5f5b4cf974cf8a6daf2a5f2c721
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-05-16 22:17:00 +0100

    UPDATE

--- src/public/scripts/src/account/activities.js
+++ src/public/scripts/src/account/activities.js
@@ -1,3 +1,3 @@
-<div>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit rem ipsam reiciendis sed, ipsa atque magnam ad sint rerum ipsum praesentium dignissimos nemo tenetur? Veniam blanditiis deleniti facilis ipsum temporibus!
-</div>
+export const Activities = {
+    template:"activiti"
+}
--- src/public/scripts/src/main.js
+++ src/public/scripts/src/main.js
@@ -1,6 +1,7 @@
 // 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
 // and then call `Vue.use(VueRouter)`.
 
+
 // 1. Define route components.
 // These can be imported from other files
 const Foo = { template: '<div>foo</div>' }
@@ -14,14 +15,16 @@ const Baz = { template: "<div>baz</div>" }
 const routes = [
     { path: '/foo', component: Foo },
     { path: '/bar', component: Bar },
-    { path: '/baz', component: Baz }
+    { path: '/baz', component: Baz },
+
 ]
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
-    routes // short for `routes: routes`
+    mode: 'history',
+    routes
 })
 
 
