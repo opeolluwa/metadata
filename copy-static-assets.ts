0d3a9cb06dd3680731b19c27a5dcb699c3b9bed2
ADEOYE ADEFEMI OPEOLUWA <67811147+opeolluwa@users.noreply.github.com>
2022-05-18 02:32:47 +0100

    Update copy-static-assets.ts

--- copy-static-assets.ts
+++ copy-static-assets.ts
@@ -6,5 +6,9 @@ shell.cp("-R", "src/public/icons", "dist/public/");
 shell.cp("-R", "src/public/stylesheets", "dist/public/");
 shell.cp("-R", "src/public/scripts", "dist/public/");
 shell.cp("-R", "src/views", "dist/");
+shell.cp("-R", "src/templates", "dist/");
+
+
+
 
 
