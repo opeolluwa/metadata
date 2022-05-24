a0e41f4c41f9eb280d0daa43f450536ee2a6a91b
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-05-24 08:51:15 +0100

    FEAT :: added uploads folser to handle taking snapshots of meta data

--- copy-static-assets.ts
+++ copy-static-assets.ts
@@ -7,6 +7,8 @@ shell.cp("-R", "src/public/stylesheets", "dist/public/");
 shell.cp("-R", "src/public/scripts", "dist/public/");
 shell.cp("-R", "src/views", "dist/");
 shell.cp("-R", "src/templates", "dist/");
+shell.cp("-R", "src/uploads", "dist/");
+
 
 
 
