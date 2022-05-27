647a0984443d026a64d0215dff485ca8bac2298b
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-05-27 13:47:51 +0100

    REFACTOR :: restructure schema design

--- src/models/Resource.ts
+++ src/models/Resource.ts
@@ -1,36 +1,34 @@
-
 import mongoose from "mongoose";
-
 const ResourceSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true,
-        lowercase:true
 
     },
     url: {
         type: String,
         required: true,
         trim: true,
-        lowercase:true
+        lowercase: true
 
     },
     description: {
         type: String,
         required: true,
         trim: true,
-        lowercase:true,
+        lowercase: true,
     },
-    iconUrl: {
+    avatarUrl: {
         type: String,
-        required: true,
+        // required: true,
         trim: true,
-        lowercase:true,
+        lowercase: true,
     },
+    category: [],
     meta: {
         like: [],
-        comment: [{ user: String, comment: String }],
+        comment: [{ username: String, comment: String }],
         clap: []
     },
 
