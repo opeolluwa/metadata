c7bc923aaa20a67b920679cf73b5619bc272ce8c
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-02 21:06:03 +0100

    UPDATE

---
+++ src/public/scripts/toggle-password-vidibility.js
@@ -0,0 +1,9 @@
+const togglePasswordButton = document.querySelector('#togglePassword');
+const password = document.querySelector('input[name="password"]');
+togglePasswordButton.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
