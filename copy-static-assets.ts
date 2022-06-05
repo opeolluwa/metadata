ddb9d376bfc69117deaa0be629d1d43393d6d9e8
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-05 22:09:22 +0100

    CONFIG :: successfully added build script of the pwa to package.json

--- copy-static-assets.ts
+++ copy-static-assets.ts
@@ -1,13 +1,17 @@
-//copy views directory content to dist directory
 
 import * as shell from "shelljs";
+//copy user account UI to dist directory
+shell.cp("-R", "./account", "dist/views/pages/");
 
+//copy views directory content to dist directory
 shell.cp("-R", "src/public/icons", "dist/public/");
 shell.cp("-R", "src/public/stylesheets", "dist/public/");
 shell.cp("-R", "src/public/scripts", "dist/public/");
 shell.cp("-R", "src/views", "dist/");
+//copy email templates  amd uploaded files path to dist directory
 shell.cp("-R", "src/templates", "dist/");
 shell.cp("-R", "src/uploads", "dist/");
+
 
 
 
