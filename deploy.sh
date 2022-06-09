e62ffc89ca875184dda2ac730868efb364e9d359
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-09 18:30:42 +0100

    UPDATE

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,25 @@
 
 
 if [ -n "$(git status --porcelain)" ]; then
-    echo -e "\e[0;31m BUILD FAILED!\e[0m\n\nYou Have uncommited changes.\n Commit them to proceed[0m";
+    echo -e "\e[0;31m BUILD FAILED!\e[0m\n\nYou Have uncommited changes.\n Commit them to proceed\e[0m";
     exit 0;
 else
-  echo -e "\e[0;34mdeploy :: ----------------> No Changes found [0m";
+  echo -e "\e[0;34mdeploy :: ----------------> No Changes found \e[0m";
 
 #enter the user account directory
-echo -e "\e[0;34mdeploy :: ----------------> Preparing To Deploy[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Preparing To Deploy\e[0m"
 cd pwa
 echo -e "\e[0;34mdeploy :: ----------------> In User Progressive Web App Directory ...\n[0m"
-echo -e "\e[0;34mdeploy :: ----------------> Installing Devdepdencies Fro Progressive Web App\n[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Installing Devdepdencies Fro Progressive Web App\n\e[0m"
 
 #update dev dependencies and dependencies
 npm install
-echo -e "\e[0;34mdeploy :: ----------------> Dependencies successfully Installed\n[0m"
-echo -e "\e[0;34mdeploy :: ----------------> Building Static Files\n[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Dependencies successfully Installed\n\e[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Building Static Files\n\e[0m"
 
 #build static files
 npm run build
-echo -e "\e[0;34mdeploy :: ----------------> Static Files Successfully Built[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Static Files Successfully Built\e[0m"
 
 #create a folder @param {account} in the src/views/pages 
 #the folder will contain to the built static files
