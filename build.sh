9d959ffe990c34efcaf9d15f63d56080a3a001c7
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-09 13:55:45 +0100

    REFACTORE

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# echo "building user-account"
-# npm run build-pwa
+echo "preparing to deploy"
+cd user-account
+echo "in user account ..."
+echo "installing devdepdencies"
+npm install
+echo "building user-account"
+npm run build
+cd ..
+echo "building user-account"
+npm run build-pwa
 echo "building user-account done"
 echo "building sass"
 npm run build-sass 
@@ -8,6 +16,6 @@ echo "building sass done"
 echo "transpiling typescript"
 npm run build-ts
 echo "build typescript done"
-# npm run copy-static-assets
+npm run copy-static-assets
 echo "build complete";
 echo "deploying to server"
