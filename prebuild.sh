61103b04d03a245b1d34d474bd145658f31af431
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-09 14:24:31 +0100

    FIXES

--- prebuild.sh
+++ prebuild.sh
@@ -8,7 +8,7 @@ echo "in user account ..."
 echo "installing devdepdencies"
 npm install
 echo "building user-account"
-npx vue-cli-service build
+npx @vue-cli-service build
 cd ..
 echo "in root directory ..."
 
