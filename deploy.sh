0be38610b4f4d43d0fb6032c396d222e4274a6a3
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-09 18:21:40 +0100

    REFACTOR

--- deploy.sh
+++ deploy.sh
@@ -4,44 +4,44 @@
 
 
 if [ -n "$(git status --porcelain)" ]; then
-    echo -e "\e[0;31m BUILD FAILED!\e[0m\n\nYou Have uncommited changes.\n Commit them to proceed";
+    echo -e "\e[0;31m BUILD FAILED!\e[0m\n\nYou Have uncommited changes.\n Commit them to proceed[0m";
     exit 0;
 else
-  echo -e "\e[0;34mdeploy :: ----------------> No Changes found ";
+  echo -e "\e[0;34mdeploy :: ----------------> No Changes found [0m";
 
 #enter the user account directory
-echo -e "\e[0;34mdeploy :: ----------------> Preparing To Deploy"
+echo -e "\e[0;34mdeploy :: ----------------> Preparing To Deploy[0m"
 cd pwa
-echo -e "\e[0;34mdeploy :: ----------------> In User Progressive Web App Directory ...\n"
-echo -e "\e[0;34mdeploy :: ----------------> Installing Devdepdencies Fro Progressive Web App\n"
+echo -e "\e[0;34mdeploy :: ----------------> In User Progressive Web App Directory ...\n[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Installing Devdepdencies Fro Progressive Web App\n[0m"
 
 #update dev dependencies and dependencies
 npm install
-echo -e "\e[0;34mdeploy :: ----------------> Dependencies successfully Installed\n"
-echo -e "\e[0;34mdeploy :: ----------------> Building Static Files\n"
+echo -e "\e[0;34mdeploy :: ----------------> Dependencies successfully Installed\n[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Building Static Files\n[0m"
 
 #build static files
 npm run build
-echo -e "\e[0;34mdeploy :: ----------------> Static Files Successfully Built"
+echo -e "\e[0;34mdeploy :: ----------------> Static Files Successfully Built[0m"
 
 #create a folder @param {account} in the src/views/pages 
 #the folder will contain to the built static files
 # once the folder has been created, copy the files recursively [(-r) means recursive]
-echo -e "\e[0;34mdeploy :: ----------------> Copying Pwa Static Files to App Directory"
+echo -e "\e[0;34mdeploy :: ----------------> Copying Pwa Static Files to App Directory[0m"
 mkdir ./../src/views/pages/account
 cp -r ./pwa/* ./../src/views/pages/account
 
-echo -e "\e[0;34mdeploy :: ----------------> Static Files Copied to App Directory"
+echo -e "\e[0;34mdeploy :: ----------------> Static Files Copied to App Directory[0m"
 
 #exit the pwa direcory
 cd ..
-echo -e "\e[0;34mdeploy :: ----------------> In Root Directory ..."
+echo -e "\e[0;34mdeploy :: ----------------> In Root Directory ...[0m"
 
 #add the changes to commit 
-echo -e "\e[0;34mdeploy :: ----------------> Adding Changes to Git Commit"
+echo -e "\e[0;34mdeploy :: ----------------> Adding Changes to Git Commit[0m"
 git add .
-git commit -m "CHORE -> added pwa static files to the app directory"
+git commit -m "CHORE -> added pwa static files to the app directory[0m"
 git push 
-echo -e "\e[0;34mdeploy :: ----------------> Changes successfully pushed to repository\nDeploying application"
+echo -e "\e[0;34mdeploy :: ----------------> Changes successfully pushed to repository\nDeploying application[0m"
 git push heroku master
 fi
