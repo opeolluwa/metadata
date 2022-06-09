bd359811d58c596806483c1dd62c597ae314fea5
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-06-09 18:23:01 +0100

    CHORE -> added pwa static files to the app directory[0m

--- deploy.sh
+++ deploy.sh
@@ -27,21 +27,21 @@ echo -e "\e[0;34mdeploy :: ----------------> Static Files Successfully Built[0m"
 #create a folder @param {account} in the src/views/pages 
 #the folder will contain to the built static files
 # once the folder has been created, copy the files recursively [(-r) means recursive]
-echo -e "\e[0;34mdeploy :: ----------------> Copying Pwa Static Files to App Directory[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Copying Pwa Static Files to App Directory\e[0m"
 mkdir ./../src/views/pages/account
 cp -r ./pwa/* ./../src/views/pages/account
 
-echo -e "\e[0;34mdeploy :: ----------------> Static Files Copied to App Directory[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Static Files Copied to App Directory\e[0m"
 
 #exit the pwa direcory
 cd ..
-echo -e "\e[0;34mdeploy :: ----------------> In Root Directory ...[0m"
+echo -e "\e[0;34mdeploy :: ----------------> In Root Directory ...\e[0m"
 
 #add the changes to commit 
-echo -e "\e[0;34mdeploy :: ----------------> Adding Changes to Git Commit[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Adding Changes to Git Commit\e[0m"
 git add .
-git commit -m "CHORE -> added pwa static files to the app directory[0m"
+git commit -m "CHORE -> added pwa static files to the app directory\e[0m"
 git push 
-echo -e "\e[0;34mdeploy :: ----------------> Changes successfully pushed to repository\nDeploying application[0m"
+echo -e "\e[0;34mdeploy :: ----------------> Changes successfully pushed to repository\nDeploying application\e[0m"
 git push heroku master
 fi
