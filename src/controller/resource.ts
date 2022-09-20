649ccad3717e844ef841b9a08661a704cb01f5d9
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-20 17:25:09 +0100

    FIXES

--- src/controller/resource.ts
+++ src/controller/resource.ts
@@ -13,52 +13,68 @@ export class ResourceControllers {
          * get the resource details from the frontend,
          * parse, validate and store it
          */
+
         const { name, description, url, category } = req.body
         const { username, user_id } = req.session.user;
-        req.params.username = username;
+
         //validate the request
-        const error = {
-            name: "",
-            description: "",
-            url: "",
-            category: ""
-        };
-
-        if (!name) {
-            error.name = "Please provide a name"
-        }
-        if (!description) {
-            error.description = "Please provide a description"
-        }
-        if (!url) {
-            error.url = "missing resource url"
-        }
-        if (url && !isValidURL(url)) {
-            error.url = "Invalid URL"
-        }
-        if (Object.values(RESOURCE_CATEGORIES).includes(category)) {
-            error.category = `invalid category, please use one of: ${Object.values(RESOURCE_CATEGORIES).join(", ")}`
-        }
-        //check for errors and send in error report if any
-        if (!Object.values(error).every(e => e === "")) {
-            return res.render("pages/dashboard", { title: "Error adding Resource ", layout: "", error: {}, value: {}, username, categories: Object.values(RESOURCE_CATEGORIES) });
-        }
-
+        /*  const error = {
+             name: "",
+             description: "",
+             url: "",
+             category: ""
+         };
+ 
+         if (!name) {
+             error.name = "Please provide a name"
+         }
+         if (!description) {
+             error.description = "Please provide a description"
+         }
+         if (!url) {
+             error.url = "missing resource url"
+         }
+         if (url && !isValidURL(url)) {
+             error.url = "Invalid URL"
+         }
+         if (Object.values(RESOURCE_CATEGORIES).includes(category)) {
+             error.category = `invalid category, please use one of: ${Object.values(RESOURCE_CATEGORIES).join(", ")}`
+         }
+         //check for errors and send in error report if any
+         if (!Object.values(error).every(e => e === "")) {
+             return res.render("pages/dashboard", { title: "Error adding Resource ", layout: "", error: {}, value: {}, username, categories: Object.values(RESOURCE_CATEGORIES) });
+         }
+  */
         try {
-            const resource = new Resource({
-                name, description, url, category
-            })
-            await resource.save()
+            const { name, description, url, category } = req.body
+            const { username, user_id } = req.session.user;
+            
+            // see if name already exist 
+            // const resourceExists = await Resource.find({ name })
+           /*  if (resourceExists) {
+                return res.render("pages/dashboard", {
+                    title: "Error adding Resource ", layout: "", error: {}, value: {}, username, categories: Object.values(RESOURCE_CATEGORIES), serverResponse: {
+                        message: `${name} already exists`,
+                        isError: true
+                    }
 
+                });
+            } */
+            const resource = new Resource({
+                name, description, url, category: [category]
+            })
+            let rr = await resource.save()
+            // console.log(rr)
             // return res.redirect("/d");
             return res.render("pages/dashboard", {
                 title: "Error adding Resource ", layout: "", error: {}, value: {}, username, categories: Object.values(RESOURCE_CATEGORIES), serverResponse: {
                     message: `${name} successfully added`,
                     isError: false
-               }
+                }
 
             });
         } catch (error) {
+            console.log(error.message)
             return res.render("pages/dashboard", {
                 title: "Error adding Resource ", layout: "", error: {}, value: {}, username, categories: Object.values(RESOURCE_CATEGORIES), serverResponse: {
                     message: "internal server error",
