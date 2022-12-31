9cb9eb97b056a7dda8f5d7dd835e1000093c1efe
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-12-31 13:14:43 +0100

    refied the UI

--- user-account/account/js/about.54763ec8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkuser_account"]=self["webpackChunkuser_account"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=n(1001),c={},r=(0,a.Z)(c,u,s,!1,null,null,null),l=r.exports}}]);
-//# sourceMappingURL=about.54763ec8.js.map
