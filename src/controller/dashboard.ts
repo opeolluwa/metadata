5acd3d986a536aac961a9c6e1a53d6145dd2ede3
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-01-02 21:08:21 +0100

    extended the ui

--- src/controller/dashboard.ts
+++ src/controller/dashboard.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { RESOURCE_CATEGORIES } from "../models/Resource";
-
+import greeting from "../lib/greetings";
 export class UserAccountViews {
     static render(req: Request, res: Response) {
         const { username, user_id } = req.session.user;
@@ -9,11 +9,19 @@ export class UserAccountViews {
         //the available category
         const categories = Object.values(RESOURCE_CATEGORIES)
         //render the view
-        return res.render("pages/dashboard", {
-            title: "Add Resource ", layout: "", error: {}, value: {}, username, categories, serverResponse: {
-                message: "",
-                isError: false
-            }
-        });
+        return res.render("pages/dashboard",
+            {
+                title: "Dashboard",
+                layout: "",
+                error: {},
+                value: {},
+                username,
+                categories,
+                greeting,
+                serverResponse: {
+                    message: "",
+                    isError: false
+                }
+            });
     }
 }
