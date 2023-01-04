ad86920e33f1e0fd756ab123a7b4e146de3f5bd9
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-01-04 01:44:31 +0100

    extended user account added boilerplater for all accont views restructure user account controller code mounted index views to routes handlers

---
+++ src/controller/dashboard/profile.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express"
+import { RESOURCE_CATEGORIES } from "../../models/Resource";
+
+export default class UserProfile {
+
+}
--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -5,4 +5,10 @@ const router = express.Router()
 
 //resister all resource route dynamically based on the router parameter
 router.get("/", /*AuthenticationMiddleware.confirm,*/ UserAccountViews.render)
+router.get("/add-resource", /*AuthenticationMiddleware.confirm,*/ UserAccountViews.addResource)
+router.get("/profile", /*AuthenticationMiddleware.confirm,*/ UserAccountViews.profile)
+router.get("/settings", /*AuthenticationMiddleware.confirm,*/ UserAccountViews.settings)
+router.get("/notifications", /*AuthenticationMiddleware.confirm,*/ UserAccountViews.notifications)
+
+
 export default router;
