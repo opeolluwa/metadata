f5066dbd9d7d26d144e5dc664879a4a90faac195
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2023-01-18 20:22:00 +0100

    updated build script

--- copy-static-assets.ts
+++ copy-static-assets.ts
@@ -2,9 +2,7 @@
 import * as shell from "shelljs";
 
 //copy views directory content to dist directory
-shell.cp("-R", "src/public/icons", "dist/public/");
-shell.cp("-R", "src/public/stylesheets", "dist/public/");
-shell.cp("-R", "src/public/scripts", "dist/public/");
+shell.cp("-R", "src/public/*", "dist/public/");
 shell.cp("-R", "src/views", "dist/");
 //copy email templates  amd uploaded files path to dist directory
 shell.cp("-R", "src/templates", "dist/");
